7c457415bc48741ae4b45b5f67838356ade14fc3
steve kim <steve.kim@aimatics.ai>
2023-01-25 17:04:57 +0900

    fix: React strict 모드 해제

--- src/components/common/GoogleMap.tsx
+++ src/components/common/GoogleMap.tsx
@@ -21,7 +21,7 @@ const GoogleMap = () => {
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        {/*<AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />*/}
       </GoogleMapReact>
     </div>
   );
